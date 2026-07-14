339aef602bc6b26d0aabe50279d7c72e28d4b5c4
Milton Quintero <quinterogiraldomilton@gmail.com>
2026-07-13 23:20:33 -0400

    Add event severity classification

--- scripts/alert_classifier.py
+++ scripts/alert_classifier.py
@@ -1,16 +1,48 @@
 from pathlib import Path
 import pandas as pd
 
+# Clasificacion de eventos por severidad
+severity = {
+    4624: "Low",
+    4625: "Medium",
+    4672: "High",
+    4688: "Medium",
+    4720: "Critical",
+    4728: "Critical",
+}        
+
 # Obtener la carpeta raíz del proyecto
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Ruta al archivo CSV
 csv_file = BASE_DIR / "data"/ "windows_security.csv"
 
+
 # Leer el archivo                                
 print(BASE_DIR)
 print(csv_file)
 df = pd.read_csv(csv_file)
 
-# Mostrar las primeras filas
-print(df.head())
+# Agregar la columna Severity
+df["Severity"] = df["EventID"].map(severity)
+
+
+print("=" * 50)
+print("SOC ALERT TRIAGE")
+print("=" * 50)
+
+print(f"\nTotal de eventos: {len(df)}")
+
+print("\nEventos encontrados:\n")
+
+print(
+    df[
+        [
+            "Timestamp",
+            "EventID",
+            "Severity",
+            "Username",
+            "SourceIP",
+        ]    
+    ]
+)    
